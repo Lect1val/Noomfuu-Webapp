bf08c2b229c32b3e8d32083a7066706d2474e6f0
Poi <o.onepiece9@gmail.com>
2021-05-12 17:43:22 +0700

    [ADD] Add route user_profile.html and  feeling_analytic.html

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('user_profile.html')
+
+
+@app.route('/analytic')
+def analytic():
+    return render_template('feeling_analytic.html')
 
 if __name__ == "__main__":
     app.debug = True
