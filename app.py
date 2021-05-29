95b9e32f6889289b1abf378273ef701fc2943ef3
Poi <o.onepiece9@gmail.com>
2021-05-29 13:49:57 +0700

    [ADD] Get contact list form firebase

--- app.py
+++ app.py
@@ -1,21 +1,49 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import render_template
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("noomfuuproject-f3b69-firebase-adminsdk-mpbf0-0db0cba4ee.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()  
+
 
 app = Flask(__name__)
 
-@app.route('/index')
+
+
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    
+    user_ref = db.collection(u'User')  
+    users = user_ref.stream()
+    nick_name_list = []
+    for user in users:
+        nick_name_list.append(u'{}'.format(user.to_dict()['nickName']))
+    return render_template('index.html', my_user_lists = nick_name_list)
+    
 
 @app.route('/profile')
 def profile():
-    return render_template('user_profile.html')
+    user_ref = db.collection(u'User')  
+    users = user_ref.stream()
+    nick_name_list = []
+    for user in users:
+        nick_name_list.append(u'{}'.format(user.to_dict()['nickName']))
+    return render_template('user_profile.html',my_user_lists = nick_name_list)
 
 
 @app.route('/analytic')
 def analytic():
-    return render_template('feeling_analytic.html')
+    user_ref = db.collection(u'User')  
+    users = user_ref.stream()
+    nick_name_list = []
+    for user in users:
+        nick_name_list.append(u'{}'.format(user.to_dict()['nickName']))
+    return render_template('feeling_analytic.html',my_user_lists = nick_name_list)
+
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = True    
     app.run()
